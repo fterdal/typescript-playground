9660786e6d114ebfc9804eba2b0fbfb341c46260
Finn <finnterdal@gmail.com>
2019-07-12 16:37:53 -0500

    delete unnecessary js files

--- src/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_typescript_1 = require("sequelize-typescript");
-exports.sequelize = new sequelize_typescript_1.Sequelize({
-    logging: false,
-    dialect: 'postgres',
-    database: 'cookie-cat',
-    models: [__dirname + '/models'],
-});
