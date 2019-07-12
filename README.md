33fed927a7a3c10ee957d5d290e6a593a1270ebb
Finn <finnterdal@gmail.com>
2019-07-12 17:07:05 -0500

    giving up on typescript and sequelize

---
+++ README.md
@@ -0,0 +1,3 @@
+# Following along with this:
+
+[Useful Tutorial](https://vivacitylabs.com/setup-typescript-sequelize/)
--- src/models/Cat.ts
+++ src/models/Cat.ts
@@ -1,2 +1,12 @@
+import * as  Sequelize from 'sequelize'
 
+export interface CatAttributes {
+  id?: number
+  name: string
+  createdAt?: Date
+  updatedAt?: Date
+}
 
+export interface CatInstance extends Sequelize.Instance<CatAttributes>, CatAttributes {
+
+}
