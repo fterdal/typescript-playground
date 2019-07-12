8b28948721ebe66a78e6c52ac339ec4aed3f0469
Finn <finnterdal@gmail.com>
2019-07-12 16:28:14 -0500

    clean up

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,1 @@
-import { Sequelize } from 'sequelize-typescript'
-
-export const sequelize = new Sequelize({
-  logging: false,
-  dialect: 'postgres',
-  database: 'cookie-cat',
-  models: [__dirname + '/models'],
-})
+import { Sequelize } from 'sequelize'
